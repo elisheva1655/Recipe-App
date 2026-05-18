522f16d45f5ac0667c4f1f1d08caf6d2e911e478
Elisheva <ekinzer@wits.edu>
2026-05-17 23:49:54 -0400

    Added product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import {Route, Routes} from 'react-router-dom'
-import { HomePage } from './pages/HomePage';
-import { ProductPage } from './pages/ProductPage';
-import { LikedPage } from './pages/LikedPage';
-import {PageNotFound} from './pages/PageNotFound';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { ProductPage } from "./pages/ProductPage";
+import { LikedPage } from "./pages/LikedPage";
+import { PageNotFound } from "./pages/PageNotFound";
+import { createContext, useContext, useState } from "react";
+import { LikedProvider } from "./components/favoritesContext";
 function App() {
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/product/:id" element={<ProductPage/>} />
-        <Route path="/favorites" element={<LikedPage/>} />
-        <Route path="*" element={<PageNotFound/>}/>
-      </Routes>
+      <LikedProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/favorites" element={<LikedPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </LikedProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
